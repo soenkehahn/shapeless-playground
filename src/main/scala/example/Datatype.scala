9abbf6210c176abde438a5ac75e9e21e7ee1fb52
Sönke Hahn <soenkehahn@gmail.com>
2020-07-31 22:42:57 -0400

    extract fieldnames

--- src/main/scala/example/Datatype.scala
+++ src/main/scala/example/Datatype.scala
@@ -1,6 +1,7 @@
 package example
 
 import shapeless._
+import shapeless.labelled._
 
 object ToDatatype {
   def datatype[T](generic: T): Datatype = ???
@@ -12,21 +13,35 @@ trait ToDatatype[T] {
 
 object Datatype {
   implicit def genericToDatatype[T, R](implicit
-      generic: Generic.Aux[T, R],
+      generic: LabelledGeneric.Aux[T, R],
       toDatatype: ToDatatype[R]
   ): ToDatatype[T] =
     new ToDatatype[T] {
       override def datatype: Datatype = toDatatype.datatype
     }
 
+  implicit def hconsToDatatype[K <: Symbol, H, Tail <: HList](implicit
+      witness: Witness.Aux[K],
+      tailToDatatype: ToDatatype[Tail]
+  ): ToDatatype[FieldType[K, H] :: Tail] =
+    new ToDatatype[FieldType[K, H] :: Tail] {
+      override def datatype: Datatype =
+        Datatype(Seq(witness.value.name) ++ tailToDatatype.datatype.fields)
+    }
+
   implicit def hnilToDatatype: ToDatatype[HNil] =
     new ToDatatype[HNil] {
-      override def datatype = Datatype("hnil")
+      override def datatype = Datatype(Seq.empty)
     }
 
   def get[T](implicit toDatatype: ToDatatype[T]): Datatype = {
     toDatatype.datatype
   }
+
+  def getFieldName[K, V](
+      value: FieldType[K, V]
+  )(implicit witness: Witness.Aux[K]): K =
+    witness.value
 }
 
-final case class Datatype(name: String)
+final case class Datatype(fields: Seq[String])
--- src/main/scala/example/Foo.scala
+++ src/main/scala/example/Foo.scala
@@ -1,8 +1,9 @@
 package example
 
+import shapeless._
 import Datatype._
 
-sealed case class Foo()
+sealed case class Foo(a: String, b: String)
 
 object Main extends App {
   println(Datatype.get[Foo])
