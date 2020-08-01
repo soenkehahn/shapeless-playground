bdd5a3210aaa212c69d872de27ee12e7c6287eec
Sönke Hahn <soenkehahn@gmail.com>
2020-07-31 22:14:52 -0400

    move generics into its own module

---
+++ src/main/scala/example/Datatype.scala
@@ -0,0 +1,32 @@
+package example
+
+import shapeless._
+
+object ToDatatype {
+  def datatype[T](generic: T): Datatype = ???
+}
+
+trait ToDatatype[T] {
+  def datatype: Datatype
+}
+
+object Datatype {
+  implicit def genericToDatatype[T, R](implicit
+      generic: Generic.Aux[T, R],
+      toDatatype: ToDatatype[R]
+  ): ToDatatype[T] =
+    new ToDatatype[T] {
+      override def datatype: Datatype = toDatatype.datatype
+    }
+
+  implicit def hnilToDatatype: ToDatatype[HNil] =
+    new ToDatatype[HNil] {
+      override def datatype = Datatype("hnil")
+    }
+
+  def get[T](implicit toDatatype: ToDatatype[T]): Datatype = {
+    toDatatype.datatype
+  }
+}
+
+final case class Datatype(name: String)
